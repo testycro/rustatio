ed399e562d6b0c125b23a83b50cc0d549181a185
testycro <testycro@tutamail.com>
2026-03-18 18:18:07 +0100

    Delete rustatio-server directory

--- rustatio-server/src/util/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Utilities module - logging and static file serving.
-
-pub mod logging;
-pub mod static_files;
-
-// Re-export commonly used types
-pub use logging::BroadcastLayer;
-pub use static_files::static_handler;
