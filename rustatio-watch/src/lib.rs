74b8a755bda79ceb43aa9d8479b9c16d52274916
testycro <testycro@tutamail.com>
2026-03-18 18:18:22 +0100

    Delete rustatio-watch directory

--- rustatio-watch/src/lib.rs
+++
@@ -1,7 +0,0 @@
-mod engine;
-mod paths;
-mod scan;
-mod types;
-
-pub use engine::{InstanceSource, InstanceState, NewInstance, WatchEngine, WatchService};
-pub use types::{EngineConfig, WatchStatus, WatchedFile, WatchedFileStatus};
