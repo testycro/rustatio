715d8c59663f2aca8e38af666404a828615250d1
testycro <testycro@tutamail.com>
2026-05-04 16:53:10 +0200

    Add tzdata package installation to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Met à jour les paquets et installe les dépendances
-apt-get update && apt-get install -y bash jq gawk sed
+apt-get update && apt-get install -y bash jq gawk sed tzdata
 
 # Exécute le script en arrière-plan
 bash /rustatio_daemon.sh &
