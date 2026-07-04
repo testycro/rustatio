cf7a9736a1710a2dfe3fb86717ecd414341d0e46
testycro <testycro@tutamail.com>
2026-07-04 10:52:45 +0200

    Update data's sample
    Removed large JSON object and replaced it with individual key-value pairs for torrent and config settings.

--- rustatio_daemon.sh
+++ rustatio_daemon.sh
@@ -1516,164 +1516,106 @@ if [[ -n "${RUSTATIO_API}" ]]; then
 fi
 
 : '
-{
-    "success": true,
-    "data": [
-        {
-            "id": "_Mf6squnrq",
-            "torrent": {
-                "info_hash": [
-                    123,
-                    123,
-                    123,
-                    123,
-                    12,
-                    123,
-                    12,
-                    12,
-                    12,
-                    123,
-                    12,
-                    12,
-                    123,
-                    123,
-                    123,
-                    123,
-                    123,
-                    123,
-                    12,
-                    12
-                ],
-                "announce": "https://test.com/announce",
-                "name": "Test.2025.MULTi.VF2.1080p.WEB.H264-SUPPLY",
-                "total_size": 1234567890,
-                "piece_length": 1234567,
-                "num_pieces": 1234,
-                "comment": "Ce torrent a été téléchargé depuis Test. https://Test.com/torrents/12345",
-                "created_by": "Edited by UNIT3D",
-                "is_single_file": false,
-                "file_count": 0
-            },
-            "config": {
-                "upload_rate": 1000,
-                "download_rate": 0.0,
-                "port": 6881,
-                "vpn_port_sync": false,
-                "client_type": "transmission",
-                "client_version": "4.0.5",
-                "initial_uploaded": 0,
-                "initial_downloaded": 0,
-                "completion_percent": 100.0,
-                "num_want": 50,
-                "randomize_rates": true,
-                "random_range_percent": 50.0,
-                "randomize_ratio": false,
-                "random_ratio_range_percent": 10.0,
-                "stop_at_ratio": null,
-                "effective_stop_at_ratio": null,
-                "stop_at_uploaded": null,
-                "stop_at_downloaded": null,
-                "stop_at_seed_time": 2678400,
-                "idle_when_no_leechers": true,
-                "idle_when_no_seeders": false,
-                "scrape_interval": 60,
-                "progressive_rates": false,
-                "target_upload_rate": 100.0,
-                "target_download_rate": 200.0,
-                "progressive_duration": 3600,
-                "post_stop_action": "idle"
-            },
-            "stats": {
-                "uploaded": 0,
-                "downloaded": 0,
-                "ratio": 0.0,
-                "left": 0,
-                "torrent_completion": 100.0,
-                "seeders": 136,
-                "leechers": 0,
-                "state": "Running",
-                "is_idling": true,
-                "idling_reason": "no_leechers",
-                "session_uploaded": 0,
-                "session_downloaded": 0,
-                "session_ratio": 0.0,
-                "elapsed_time": {
-                    "secs": 123,
-                    "nanos": 123
-                },
-                "current_upload_rate": 0.0,
-                "current_download_rate": 0.0,
-                "average_upload_rate": 0.0,
-                "average_download_rate": 0.0,
-                "upload_progress": 0.0,
-                "download_progress": 0.0,
-                "ratio_progress": 0.0,
-                "seed_time_progress": 0.0,
-                "effective_stop_at_ratio": null,
-                "eta_ratio": null,
-                "eta_uploaded": null,
-                "eta_seed_time": {
-                    "secs": 123,
-                    "nanos": 0
-                },
-                "eta_download_completion": null,
-                "upload_rate_history": [],
-                "download_rate_history": [],
-                "ratio_history": [],
-                "history_timestamps": [],
-                "stop_condition_met": false,
-                "post_stop_action": "idle",
-                "announce_count": 1
-            },
-            "created_at": 1771234567,
-            "source": "watch_folder",
-            "tags": [
-                "Test",
-                "Forced"
-            ]
-        }
-    ]
-}
-'
+id : _Mf6squnrq
 
-: '
-{
-    "instances": {},
-    "default_config": {
-        "upload_rate": 5000.0,
-        "download_rate": 0.0,
-        "port": 6881,
-        "vpn_port_sync": false,
-        "client_type": "transmission",
-        "client_version": null,
-        "initial_uploaded": 0,
-        "initial_downloaded": 0,
-        "completion_percent": 100.0,
-        "num_want": 50,
-        "randomize_rates": true,
-        "random_range_percent": 10.0,
-        "randomize_ratio": false,
-        "random_ratio_range_percent": 10.0,
-        "stop_at_ratio": null,
-        "effective_stop_at_ratio": null,
-        "stop_at_uploaded": null,
-        "stop_at_downloaded": null,
-        "stop_at_seed_time": null,
-        "idle_when_no_leechers": false,
-        "idle_when_no_seeders": false,
-        "scrape_interval": 60,
-        "progressive_rates": false,
-        "target_upload_rate": null,
-        "target_download_rate": null,
-        "progressive_duration": 3600,
-        "post_stop_action": "idle"
-    },
-    "default_preset": null,
-    "watch_settings": {
-        "max_depth": 1,
-        "auto_start": true
-    },
-    "custom_presets": [],
-    "version": 1
-}
+torrent.info_hash : "123"
+torrent.announce : "https://test.com/announce"
+torrent.name : "Test.2025.MULTi.VF2.1080p.WEB.H264-SUPPLY"
+torrent.total_size : 1234567890
+torrent.piece_length : 1234567
+torrent.num_pieces : 1234
+torrent.comment : "Ce torrent a été téléchargé depuis Test. https://Test.com/torrents/12345"
+torrent.created_by : "Edited by UNIT3D"
+torrent.is_single_file : false
+torrent.file_count : 0
+
+config.upload_rate : 1000
+config.download_rate : 0.0
+config.port : 6881
+config.vpn_port_sync : false
+config.client_type : "transmission"
+config.client_version : "4.0.5"
+config.initial_uploaded : 0
+config.initial_downloaded : 0
+config.completion_percent : 100.0
+config.num_want : 50
+config.randomize_rates : true
+config.random_range_percent : 50.0
+config.randomize_ratio : false
+config.random_ratio_range_percent : 10.0
+config.stop_at_ratio : null
+config.effective_stop_at_ratio : null
+config.stop_at_uploaded : null
+config.stop_at_downloaded : null
+config.stop_at_seed_time : 2678400
+config.idle_when_no_leechers : true
+config.idle_when_no_seeders : false
+config.scrape_interval : 60
+config.progressive_rates : false
+config.target_upload_rate : 100.0
+config.target_download_rate : 200.0
+config.progressive_duration : 3600
+config.post_stop_action : "idle"
+
+stats.uploaded : 0
+stats.downloaded : 0
+stats.ratio: 0.0
+stats.left: 0
+stats.torrent_completion : 100.0
+stats.seeders : 136
+stats.leechers : 0
+stats.state : "Running"
+stats.is_idling : true
+stats.idling_reason : "no_leechers"
+stats.session_uploaded : 0
+stats.session_downloaded : 0
+stats.session_ratio : 0.0
+stats.current_upload_rate : 0.0
+stats.current_download_rate : 0.0
+stats.average_upload_rate : 0.0
+stats.average_download_rate : 0.0
+stats.upload_progress : 0.0
+stats.download_progress : 0.0
+stats.ratio_progress : 0.0
+stats.seed_time_progress : 0.0
+stats.effective_stop_at_ratio : null
+stats.eta_ratio : null
+stats.eta_uploaded : null
+stats.eta_download_completion : null
+stats.stop_condition_met : false
+stats.post_stop_action : "idle"
+stats.stats.announce_count : 1
+
+tags : [
+    "Test",
+    "Forced"
+]
+
+default_config.upload_rate : 5000.0
+default_config.download_rate : 0.0
+default_config.port : 6881
+default_config.vpn_port_sync : false
+default_config.client_type : "transmission"
+default_config.client_version : null
+default_config.initial_uploaded : 0
+default_config.initial_downloaded : 0
+default_config.completion_percent : 100.0
+default_config.num_want : 50
+default_config.randomize_rates : true
+default_config.random_range_percent : 10.0
+default_config.randomize_ratio : false
+default_config.random_ratio_range_percent : 10.0
+default_config.stop_at_ratio : null
+default_config.effective_stop_at_ratio : null
+default_config.stop_at_uploaded : null
+default_config.stop_at_downloaded : null
+default_config.stop_at_seed_time : null
+default_config.idle_when_no_leechers : false
+default_config.idle_when_no_seeders : false
+default_config.scrape_interval : 60
+default_config.progressive_rates : false
+default_config.target_upload_rate : null
+default_config.target_download_rate : null
+default_config.progressive_duration : 3600
+default_config.post_stop_action : "idle"
 '
