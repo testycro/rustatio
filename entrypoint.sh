08f8edc1babbbb435f22450ca1a6213223314730
testycro <testycro@tutamail.com>
2026-07-14 15:21:29 +0200

    Enhance entrypoint.sh with tini for process management
    Added 'tini' to the package installation and updated the exec command to use 'tini' for process management.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 # Met à jour les paquets et installe les dépendances
-apt-get update && apt-get install -y bash jq gawk sed tzdata
+apt-get update && apt-get install -y bash jq gawk sed tzdata tini
 
 # Exécute le script en arrière-plan
 bash /rustatio_daemon.sh &
 
 # Lance le processus principal
-exec /app/entrypoint.sh /app/rustatio-server "$@"
+exec tini -g -- /app/entrypoint.sh /app/rustatio-server "$@"
+#exec /app/entrypoint.sh /app/rustatio-server "$@" > >(tee -a /data/rustatio-server.log) 2> >(tee -a /data/rustatio-server.log >&2)
